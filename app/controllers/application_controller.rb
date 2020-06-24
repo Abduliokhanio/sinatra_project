7fe45d7354e86533deb892d10bc0f51bb12a314e
Abdul Khan <akhan17@outlook.com>
2020-06-24 00:23:22 -0500

    login routes set up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,12 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "f650ed69344bab0084199bb8cc9aa5a1bd6756c3b57ad67023255af0fc3795057e"
   end
 
   get "/" do
-    erb :'welcome'
+    #erb :'welcome'
   end
 
   get "/tickets" do #Read
--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,7 +1,21 @@
 class LoginController < ApplicationController
 
     get "/login" do 
-        "login form"
+        erb :'sessions/login'
     end
+
+    post '/login' do 
+        @user = Employee.find_by(username: params[:username])
+        erb :'tickets/all_tickets'
+    end 
+
+    get "/signup" do 
+        erb :'sessions/signup'
+    end 
+
+    post "/signup" do 
+        #@user = Employee.create()#psudo bs code
+    end 
+
 
 end 
